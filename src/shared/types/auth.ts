905cf65187ed0559c2d96403fbce87c9f7eb6bfe
Валентин Петров <BestVell@mail.ru>
2025-12-04 16:39:15 +0700

    #1.1 Сделал форму для авторизации с mui

---
+++ src/shared/types/auth.ts
@@ -0,0 +1,28 @@
+import {ReactNode} from "react";
+
+export interface AuthData{
+    login: string;
+    password: string;
+}
+
+export interface AuthFormProps {
+    authData:AuthData,
+    handlePasswordFieldChange:(payload:string)=>void,
+    handleLoginFieldChange:(payload:string)=>void,
+}
+
+export interface UserData {
+    token: string;
+    role: string;
+}
+
+export interface AuthContextType {
+    isAuth: boolean;
+    isLoadingAuth: boolean;
+    userData: UserData | null;
+    loginUser: (authData: AuthData) => Promise<any>;
+}
+
+export interface AuthProviderProps {
+    children: ReactNode;
+}
