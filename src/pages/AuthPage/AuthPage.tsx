f61f9f9a61d05218a3b08f36bb2ed371dfc70d41
Валентин Петров <BestVell@mail.ru>
2026-05-21 10:10:00 +0700

    Финальные исправления №4 Убрал методы регистрации, оставил только вход

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -18,14 +18,14 @@ export const AuthPage: FC = () => {
         <div className={styles.formAndButtonBox}>
             <AuthForm registration={registration}/>
             <div className={styles.testCredentials}>
-                <div className={styles.links}>
-                    <a className={styles.switchLink} onClick={changeForm} role="button">
-                        {registration ? 'Авторизация' : 'Регистрация'}
-                    </a>
-                </div>
-                <p>Тестовые учетные записи:</p>
-                <p>email1 / password</p>
-                <p>email2 / password</p>
+                {/*<div className={styles.links}>*/}
+                {/*    <a className={styles.switchLink} onClick={changeForm} role="button">*/}
+                {/*        {registration ? 'Авторизация' : 'Регистрация'}*/}
+                {/*    </a>*/}
+                {/*</div>*/}
+                {/*<p>Тестовые учетные записи:</p>*/}
+                {/*<p>email1 / password</p>*/}
+                {/*<p>email2 / password</p>*/}
             </div>
         </div>
     )
